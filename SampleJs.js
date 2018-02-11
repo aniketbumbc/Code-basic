a3ce803660ec2c75abf2f6ece3349fee46e2c96e
aniket umbc <aniket.umbc@gmail.com>
2018-02-11 16:34:06 +0530

    object Add

---
+++ SampleJs.js
@@ -0,0 +1,56 @@
+console.log("Hello");
+
+var a ="learn Hard";
+
+//var intake = prompt(a);
+
+document.write("Hello From Java Script " +a);
+
+
+var person={ 
+    first_name:'Aniket',
+    last_name:'Bunny',
+    age:33,city:'Paris',
+    country:'France',
+    university:'Paris University'
+};
+
+
+var Studnet ={
+    student1:{
+        name:'Aniket',
+        age:33
+    },
+    student2:{
+        name:'Bunny',
+        age:23
+    },
+    student3:{
+        name:'Maggi',
+        age:33
+    },
+    student4:{
+        name:'banna',
+        age:55
+    },
+    student5:{
+        name:'Maggi-Bom',
+        age:45
+    },
+    student6:{
+        name:'banna-mom',
+        age:55
+    }
+};
+
+var b=1;
+
+console.log(Studnet['student' + b ]);
+b++;
+console.log(Studnet['student' + b]);
+b++;
+console.log(Studnet['student' + b]);
+b++;
+console.log(Studnet['student' + b]);
+b++;
+console.log(Studnet['student' + b]);
