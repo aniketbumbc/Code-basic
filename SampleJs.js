24311e8a51a55138155bff1d3920bc5d5c73e87b
aniket umbc <aniket.umbc@gmail.com>
2018-02-11 17:23:59 +0530

    Array method more

--- SampleJs.js
+++ SampleJs.js
@@ -58,7 +58,7 @@ var b=1;
 // array in java script 
 
 var nameCity ;
-var school=["aniket","Bunny","Mike","Marker","Goom"];
+var school=["aniket",1,46,33,8,32,66,"Bunny","Mike","Marker","Goom"];
 
 school.push("Hello New");
 console.log(school);
@@ -81,4 +81,8 @@ var hk=school.join('*');
 
 var NewSchool =school.slice(3);
 
-console.log(NewSchool);;
+console.log(NewSchool.sort());
+console.log(NewSchool.reverse());
+
+//sorting in array
+
