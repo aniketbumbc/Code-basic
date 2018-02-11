50c829a13f696cb49866f2c1a0963d580e083d6b
aniket umbc <aniket.umbc@gmail.com>
2018-02-11 23:58:32 +0530

    Loop Done

--- condition.js
+++ condition.js
@@ -34,3 +34,63 @@ switch(a){
     default:
     console.log('No valid case found');
 }
+
+// Loops Do while loop
+
+// while loop dose not check conditon true
+var person ={
+name:'aniket',
+name1:'Bunny',
+name2:'Bun-Bun',
+name3:'Mascco',
+name4:'Buunty'
+};
+
+var a = 1;
+
+while ( a < 5)
+{
+    console.log (person['name'+ a] );
+    a++;
+}
+
+// var b=1;
+// while(b<10)
+// {
+//     console.log(b);
+//     b++;
+// }
+
+
+// do -while loop check condition true  ( check conditon before )
+
+// var c=1;
+
+// do{
+//     console.log(c++);
+// }
+// while ( c < 5);
+
+// For Loops 
+
+for(var k=10; k>=1; k--)
+{
+    console.log(k);
+}
+// for in loop 
+for(a in person)
+{
+    console.log (person[a] );
+}
+debugger
+var student=['abc','pqe','xyz','hell','Mike']
+for(z = 0; z < student.length; z++)
+{
+    console.log(student[z]);
+    console.log(typeof z);
+}
+
+for(x of student)
+{
+    console.log(x);
+}
