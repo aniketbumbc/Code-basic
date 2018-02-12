2678836c40bc3821fc192612946dc32d49cc8a82
aniket umbc <aniket.umbc@gmail.com>
2018-02-12 08:35:11 +0530

    Simple Function with parameter array

--- function.js
+++ function.js
@@ -12,3 +12,25 @@ function myFunpara(a) {
     console.log(a);
 }
 myFunpara("Hello from variable");
+
+
+//multiple argument function
+
+function additon(a, b) {
+    return a + b;
+}
+console.log(additon(12, 33));
+
+//arry function
+
+var person = ['aniket', 'abhi', 'mike', 'king'];
+function addArray(personadd, addNew) {
+    personadd.push(addNew);
+    console.log(personadd);
+    
+    return "Adding New Array";
+}
+
+addArray(person, "New item");
+
+//gobal and local variable scope 
