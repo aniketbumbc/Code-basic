69bf40290160ca071fca19df59a8d206e9b9f03e
aniket umbc <aniket.umbc@gmail.com>
2018-02-12 08:17:01 +0530

    Simple Function

---
+++ function.js
@@ -0,0 +1,14 @@
+console.log("Function Coming Soon");
+
+
+//function no parameter 
+function myFun() {
+    console.log("Welcome to Function");
+}
+myFun();
+
+// function with parameter 
+function myFunpara(a) {
+    console.log(a);
+}
+myFunpara("Hello from variable");
