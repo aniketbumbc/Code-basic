66420181e2288e81b25d3f3cfdf50da61553c121
aniket umbc <aniket.umbc@gmail.com>
2018-02-18 12:49:15 +0530

    Controller Study

--- Script-angular.js
+++ Script-angular.js
@@ -1,1 +1,28 @@
-// create a module 
+// create a module and register controller 
+
+
+// create module with name myModule
+var myApp = angular.module("myModule",[]);  //myApp module object
+
+// var myController = function($scope){
+//     $scope.message="Hello Angular World";
+// };
+
+
+// // to register controller with module
+// myApp.controller("myController", myController);  //(name of controller ,name controller function)
+
+
+// create  controller 
+myApp.controller("myController", function($scope){  // create controller function  and register with module 
+    $scope.message = "Hello Angular World";
+   
+//debugger;
+    var employee = {
+        firstName: "aniekt",
+        lastName: "KBD",
+        gender: "Male"
+    };
+    $scope.employee = employee;
+});
+
